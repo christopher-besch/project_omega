6633d45d345ab6115e428415bdf9538a52bae79e
Christopher Besch <christopher.besch@gmx.de>
2021-03-30 10:05:22 +0200

    added config

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
+    user = {"username": "Chris"}
+    return render_template("index.html", title="Home", user=user)
